8ea4989934a448c434a49d92bead67f996859d0c
ardelon <akeskinw@gmail.com>
2022-07-30 11:03:16 +0300

    -

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 const PORT = 5000
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'));
 
-app.get("", (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'));
-})
+// app.get("", (req, res) => {
+//     res.sendFile(path.join(__dirname, '/index.html'));
+// })
 
 app.listen(PORT);
 console.log('Listening...');
