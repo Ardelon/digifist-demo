f0fbc5fc5ca5dc5ccb579ae703b8878fc7acb1f6
ardelon <akeskinw@gmail.com>
2022-07-30 12:22:35 +0300

    -

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
-const PORT = 5000
+require('dotenv').config()
 app.use(express.static('public'));
 
-app.get("", (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-})
+app.get('', (req, res) => {
+	res.sendFile(path.join(__dirname, 'index.html'));
+});
 
-app.listen(PORT);
-console.log('Listening...');
+const server = app.listen(process.env.PORT || 5000, () => {
+	var port = server.address().port;
+	console.log('Listening...' + port);
+});
